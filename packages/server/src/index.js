77974e18b071284beb63b7eb74352a28e93f3f47
Blaevoet Benoit <blaevoet.benoit@outlook.com>
2022-12-12 10:48:23 +0100

    config lerna /start

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,2 +1,16 @@
-const test = 36
-console.log(test)
+const fastify = require('fastify')({ logger: true })
+
+fastify.get('/', async (req, rep) => {
+  return { hello: 'world' }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 }, console.log('server started'))
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
