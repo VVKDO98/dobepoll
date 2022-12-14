bda4d0d086395d94deaa45dc406f16c028289109
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-12-14 11:27:42 +0100

    Refactor button component

--- packages/app/src/components/Button.jsx
+++ packages/app/src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Button = ({ content, link, secondary }) => {
+const Button = ({ nolink, content, link, secondary, event }) => {
+  if (nolink) {
+    return <button
+              className={secondary ? 'w-1/3 h-10 bg-slate-500 rounded-md font-semibold' : 'w-1/3 h-10 bg-sky-500 rounded-md font-semibold'}
+              onClick={() => console.log('Herlo')}
+            >
+              {content}
+            </button>
+  }
+
   return (
     <button className={secondary ? 'w-1/3 h-10 bg-slate-500 rounded-md font-semibold' : 'w-1/3 h-10 bg-sky-500 rounded-md font-semibold'}>
       <Link to={link}>{content}</Link>
--- packages/app/src/components/CreatePollComp.jsx
+++ packages/app/src/components/CreatePollComp.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
+import Button from './Button'
 import Input from './Input'
 
 const CreatePollComp = () => {
   return (
     <div className='w-full p-5 dark:bg-slate-600 rounded-md'>
-      <form action="" className='w-full h-full'>
-        <Input name={'Title'} value={'title'}/>
-        <Input name={'Description'} value={'description'}/>
-        <div className='flex flex-col'>
-          <p className='text-lg'>Options</p>
-          <button className='py-1 px-3 bg-slate-500 rounded-md font-semibold'>Add</button>
-        </div>
-        <button type='submit' className='py-1 px-3 bg-sky-500 rounded-md font-semibold'>Create poll</button>
-      </form>
+      <Input name={'Title'} value={'title'}/>
+      <Input name={'Description'} value={'description'}/>
+      <div className='mb-5 flex flex-col'>
+        <p className='text-lg'>Options</p>
+        <Button nolink={true} content={'Add'} event={''} secondary={true}/>
+      </div>
+      <Button nolink={true} content={'Create poll'} event={''} secondary={false}/>
     </div>
   )
 }
