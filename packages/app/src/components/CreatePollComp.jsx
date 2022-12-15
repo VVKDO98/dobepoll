acdccc3c3c8fa17532e3b47371f1b50d1c2d696d
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-12-15 14:12:50 +0100

    Create form and post data in db

--- packages/app/src/components/CreatePollComp.jsx
+++ packages/app/src/components/CreatePollComp.jsx
@@ -1,19 +1,74 @@
-import React from 'react'
-import Button from './Button'
-import Input from './Input'
+import React, { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+import { Formik, Field, Form } from 'formik'
+import { useNavigate } from 'react-router-dom'
 
-const CreatePollComp = () => {
+const POST_POLL = gql`
+mutation Mutation($poll: PollInput!) {
+  addPollWithOptions(poll: $poll) {
+    poll {
+      id
+      description
+      name
+      options {
+        name
+      }
+      id
+      created_at
+    }
+  }
+}
+`
+
+const FormikForm = () => {
+  const [count, setCount] = useState(0)
+  const [addPollWithOptions, { data, loading, error }] = useMutation(POST_POLL)
+  const navigate = useNavigate()
+
   return (
-    <div className='w-full p-5 dark:bg-slate-600 rounded-md'>
-      <Input name={'Title'} value={'title'}/>
-      <Input name={'Description'} value={'description'}/>
-      <div className='mb-5 flex flex-col'>
-        <p className='text-lg'>Options</p>
-        <Button nolink={true} content={'Add'} event={() => console.log('hello')} secondary={true}/>
-      </div>
-      <Button nolink={true} content={'Create poll'} event={() => console.log('hello')} secondary={false}/>
-    </div>
+    <Formik
+      initialValues={{
+        name: '',
+        description: '',
+        options: [{ name: '' }]
+      }}
+      onSubmit={async (values, { resetForm }) => {
+        const resp = await addPollWithOptions({ variables: { poll: values } })
+        console.log(resp)
+        resetForm()
+        navigate(`/poll/${resp.data.addPollWithOptions.poll.id}`)
+      }}
+    >
+      {({ values }) => (
+        <Form>
+          <label htmlFor="name">Name</label>
+          <Field type="text" name="name" />
+
+          <label htmlFor="description">Description</label>
+          <Field type="text" name="description" />
+
+          {values.options.map((item, idx) => (
+            <div key={`name-${idx}`}>
+              <label htmlFor={`name-${idx}`}>Option</label>
+              <Field type="text" name={`options[${idx}].name`} />
+            </div>
+          ))}
+
+          <button
+            type="button"
+            onClick={() => {
+              values.options.push({ name: '' })
+              setCount(count + 1)
+            }}
+          >
+            Add Name
+          </button>
+
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
-export default CreatePollComp
+export default FormikForm
--- packages/app/src/components/Input.jsx
+++ packages/app/src/components/Input.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const Input = ({ name, value }) => {
+const Input = ({ name, value, setState }) => {
   return (
     <div className='mb-5 flex flex-col'>
-      <label htmlFor={value} className='text-lg'>{name}</label>
-      <input type="text" name={value} className='dark:bg-slate-800 border-none rounded-md'/>
+      <input type="text" name={value} className='dark:bg-slate-800 border-none rounded-md' onChange={setState}/>
     </div>
   )
 }
