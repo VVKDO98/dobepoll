1a341b929dd7f3f25c716084fc1fbd5cc7d2b373
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-12-16 14:24:19 +0100

    Vote

--- packages/app/src/components/PollComp.jsx
+++ packages/app/src/components/PollComp.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { useQuery, gql } from '@apollo/client'
+import { useMutation, useQuery, gql } from '@apollo/client'
 import Button from './Button'
 import PollOption from './PollOption'
 import { formatDistance } from 'date-fns'
@@ -20,17 +20,40 @@ query GetPollByID($id: Int) {
 }
 `
 
+const POST_VOTE = gql`
+mutation Mutation($vote: VoteInput!) {
+  vote(vote: $vote)
+}
+`
+
 const PollComp = () => {
   const { id } = useParams()
   const paramsId = parseInt(id)
   const { loading, error, data } = useQuery(GET_POLL_BY_ID, {
     variables: { id: paramsId }
   })
+  const [vote, { loading: loadingVote, error: errorVote, data: dataVote }] = useMutation(POST_VOTE)
+
+  const [optionValue, setOptionValue] = useState(0)
 
   if (loading) return <p>Loading ...</p>
   if (error) return <p>Error ...</p>
 
-  console.log(data)
+  const handleVote = async () => {
+    if (optionValue === 0) return
+    const val = optionValue
+    setOptionValue(0)
+    const resp = await vote({
+      variables: {
+        vote: {
+          options_id: parseInt(val),
+          polls_id: parseInt(id)
+        }
+      }
+    })
+    console.log(resp)
+    return resp
+  }
 
   return (
     <div className='w-full p-5 dark:bg-slate-600 rounded-md'>
@@ -41,10 +64,10 @@ const PollComp = () => {
       </div>
       <div className='mb-10'>
         <h3 className='mb-1 text-base font-semibold xl:text-xl'>Options</h3>
-        {data.poll.options.map((option) => <PollOption key={option.id} id={option.id} name={option.name}/>)}
+        {data.poll.options.map((option) => <PollOption key={option.id} id={option.id} name={option.name} setOptionValue={setOptionValue}/>)}
       </div>
       <div className='w-full flex items-center justify-between gap-2'>
-        <Button content={'Vote'} link={'/'}/>
+        <Button content={'Vote'} event={handleVote}/>
         <Button content={'Results'} link={`/poll/${id}/result`} secondary={true}/>
         <Button content={'Share'} link={'/'} secondary={true}/>
       </div>
