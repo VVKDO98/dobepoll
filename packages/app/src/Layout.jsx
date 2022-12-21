64ad7f7a71bc0f02fc156bb8a330431da72ebc77
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-12-21 11:57:06 +0100

    fix screen

--- packages/app/src/Layout.jsx
+++ packages/app/src/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <>
       <div
         className='
-        w-screen h-screen relative
+        w-screen min-h-screen relative
         text-slate-900 dark:text-slate-50
         bg-white dark:bg-slate-800'>
         <main className='w-4/5 h-full mx-auto lg:w-3/4 2xl:w-2/3'>
